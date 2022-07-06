6a78ba2ee445caf4793dd566b9b6f3287355bfce
Julian Kominovic <juliankominovic@gmail.com>
2022-07-05 21:22:29 -0300

    #FEAT: rollup build with all deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 // Contents of the file /rollup.config.js
+import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
 const config = [
   {
     input: "src/index.ts",
@@ -8,7 +12,18 @@ const config = [
       format: "cjs",
       sourcemap: true,
     },
-    plugins: [typescript()],
+    plugins: [
+      typescript(),
+      commonjs(),
+      json(),
+      nodeResolve({
+        moduleDirectories: [
+          "node_modules",
+          "node_modules/.pnpm",
+          "node_modules/.pnpm/node_modules",
+        ],
+      }),
+    ],
   },
 ];
 export default config;
